14fc5572a2425e9e3339fc4c8ca0f7c06c7525c6
Marcin Galeziowski <marcin.galeziowski@gmail.com>
2026-08-26 11:59:08 +0200

    fix: prisma.config.ts ) fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
@@ -6,7 +7,10 @@ export default defineConfig({
     path: 'prisma/migrations',
   },
   datasource: {
-    // Wklejamy pełny adres URL jako zwykły tekst w pojedynczych cudzysłowach:
-    url: 'postgresql://admin:YourSuperStrongProductionPassword123!@postgres:5432/nest_db?schema=public',
+    // 1. Najpierw spróbuje przeczytać zmienną (dla CI / lokalnie)
+    // 2. Jeśli zmienna jest pusta, użyje adresu produkcyjnego na EC2 jako fallback (po znaku ||)
+    url:
+      process.env.DATABASE_URL ||
+      'postgresql://admin:YourSuperStrongProductionPassword123!@postgres:5432/nest_db?schema=public',
   },
 });
